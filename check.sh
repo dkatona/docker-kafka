fa79f4e59e6583f74f5a53a7b41fc149580f2f3a
freignat <freignat@axway.com>
2016-06-28 08:07:55 +0200

    fix issue 257

--- check.sh
+++ check.sh
@@ -5,21 +5,21 @@ set -euo pipefail
 #TOPIC_LIST="amp-logs amp-service-start amp-service-stop amp-service-terminate amp-docker-events amp-service-events"
 
 if [ -f /tmp/kafka-topics ]; then
-	exit 0
+    pidof java
+else
+    echo "Checking topic list..."
+    list=$(bin/kafka-topics.sh --zookeeper $ZOOKEEPER_CONNECT --list)
+    echo "Checking topic list : $list"
+    for topic in $TOPIC_LIST
+    do
+        if [[ $list =~ ^.*$topic ]]; then
+            echo "$topic exists"
+        else
+            echo "Creating topic $topic..."
+            bin/kafka-topics.sh --zookeeper $ZOOKEEPER_CONNECT --create --partitions=1 --replication-factor=1 --topic $topic
+            echo "Creating topic $topic Done"
+        fi
+    done
+    touch /tmp/kafka-topics
 fi
-
-echo "Checking topic list..."
-list=$(bin/kafka-topics.sh --zookeeper $ZOOKEEPER_CONNECT --list)
-echo "Checking topic list : $list"
-for topic in $TOPIC_LIST
-do
-	if [[ $list =~ ^.*$topic ]]; then
-		echo "$topic exists"
-	else
-		echo "Creating topic $topic..."
-		bin/kafka-topics.sh --zookeeper $ZOOKEEPER_CONNECT --create --partitions=1 --replication-factor=1 --topic $topic
-		echo "Creating topic $topic Done"
-	fi
-done
-touch /tmp/kafka-topics
 
